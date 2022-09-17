900aa10945b693bbcb239a59cc899be0b4bea5e6
codesari <ahmtsr81@gmail.com>
2022-09-17 11:11:10 +0300

    task-list

---
+++ todo.js
@@ -0,0 +1,8 @@
+const todoForm = document.querySelector(".todo-form");
+const todoInput = document.querySelector(".todo-input");
+const ul = document.querySelector(".task-item");
+
+// ! array which stores every task
+let task = [];
+
+// ! add an eventListener on form,and listen for submit event
